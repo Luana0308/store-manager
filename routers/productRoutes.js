0d9cc4949772829de21dd71ffcd09252b38a308c
Luana <luanaff-2008@hotmail.com>
2022-05-26 15:57:13 -0300

    fix: conserta erro de lint

--- routers/productRoutes.js
+++ routers/productRoutes.js
@@ -1,8 +1,8 @@
 const express = require('express');
 
 const router = express.Router();
-const { validateName } = require('../middlewares/validateName');
-const { validateQuantity } = require('../middlewares/validateQuantity');
+// const { validateName } = require('../middlewares/validateName');
+// const { validateQuantity } = require('../middlewares/validateQuantity');
 const { getAllProducts, 
         getProductsById, 
         createProduct } = require('../controllers/productController');
