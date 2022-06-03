cb19aff8f7b3174e8638e327faa0ee7ab5169e56
Luana <luanaff-2008@hotmail.com>
2022-06-03 12:25:01 -0300

    feat: testes model 85% de cobertura

--- test/unit/models/productsModels.js
+++ test/unit/models/productsModels.js
@@ -1,11 +1,15 @@
 const sinon = require('sinon');
 const { expect } = require('chai');
-const { getAllProducts } = require('../../../models/productsModel');
+const { getAllProducts, 
+        getProductsById, 
+        createProduct,
+        updateProduct, 
+        deleteProduct} = require('../../../models/productsModel');
 const connection = require('../../../models/connection');
 
 describe('busca todos os produtos no banco de dados', () => {
     
-    describe('verifica quando tem produtos no banco de dados', () => {
+    describe('getAllProducts', () => {
 
         before( async () => {
             sinon.stub(connection, 'execute').resolves([
@@ -33,7 +37,7 @@ describe('busca todos os produtos no banco de dados', () => {
             expect(resultado).to.includes.all.keys("id", "name", "quantity");
         });
     })
-    describe('verifica quando nao tem produtos', () => {
+    describe('getAllProducts vazio', () => {
 
         before( async () => {
             sinon.stub(connection, 'execute').resolves([{}])
@@ -50,5 +54,105 @@ describe('busca todos os produtos no banco de dados', () => {
             const [resultado] = await getAllProducts(); 
             expect(resultado).to.be.empty;
         });
-    })
+    });
+
+    describe('getProductsById' ,() => {
+        before( async () => {
+            sinon.stub(connection, 'execute').resolves(
+                {
+                    "id": 1,
+                    "name": "Martelo de Thor",
+                    "quantity": 10,
+                }
+        )
+        });
+        after(() => {
+            connection.execute.restore();
+        })
+      
+        it('verfica se a função retorna um objeto e se não está vazio', async () => {
+            const response = await getProductsById(1)
+            expect(response).to.be.an('object');
+            expect(response).not.be.empty;
+        })
+    });
+
+    describe('createProduct',() => {
+        const product =  {
+            "name": "Martelo de Thor",
+            "quantity": 10,
+        }
+
+        before(async () => {
+            sinon.stub(connection, 'execute').resolves( {
+                "id": 1,
+                "name": "Martelo de Thor",
+                "quantity": 10,
+            });
+          });
+    
+          after(async () => {
+            connection.execute.restore();
+          });
+
+          it('retorna um objeto a função', async () => {
+            const response = await createProduct(product);
+     
+            expect(response).to.be.a('object');
+          });
+     
+          it('tal objeto possui o "id" do novo produto inserido', async () => {
+            const response = await createProduct(product);
+     
+            expect(response).to.have.a.property('id');
+          });
+
+    });
+
+    describe('updateProduct',() => {
+        const product =  {
+            "id": 1,
+            "name": "Martelo de Thor",
+            "quantity": 10,
+        }
+
+        before(async () => {
+            sinon.stub(connection, 'execute').resolves( {
+                "id": 1,
+                "name": "Martelo de Thor",
+                "quantity": 10,
+            });
+          });
+    
+          after(async () => {
+            connection.execute.restore();
+          });
+
+        it('verifica se a função retorna um objeto e se possui as propriedades "id", "name" e "quantity"', async () => {
+            const response = await updateProduct(product);
+            expect(response).to.be.an('object');
+            expect(response).to.includes.all.keys("id", "name", "quantity");
+        });
+    });
+
+    describe('deleteProduct' ,() => {
+        const product =  {
+            "id": 1,
+            "name": "Martelo de Thor",
+            "quantity": 10,
+        }
+
+        before(async () => {
+            sinon.stub(connection, 'execute').resolves({});
+          });
+    
+          after(async () => {
+            connection.execute.restore();
+          });
+
+        it('verifica se a função retorna um objeto vazio', async () => {
+            const response = await deleteProduct(product);
+            expect(response).to.be.empty;
+        });
+    });
 })
